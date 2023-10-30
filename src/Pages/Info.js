6a881c122f8326e0f1bd0a72092da5a000c6d2a1
D4RKN1K3 <nicolas.barraza.cortes@alumnos.uta.cl>
2023-10-30 18:08:03 -0300

    Update Info.js

--- src/Pages/Info.js
+++ src/Pages/Info.js
@@ -69,149 +69,148 @@ export const Info = () => {
       };
       
 
-      return (
-        <div
-        className="mx-auto max-w-lg font-normal"
+    return (
+      <div
+      className="mx-auto max-w-lg font-normal"
+      >
+        <h1>Datos del usuario</h1>
+        {isEditing ? (
+        <form
+        className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
+        id="borderimg1"
         >
-          <h1>Datos del usuario</h1>
-          {isEditing ? (
-            <form
-            className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
-            id="borderimg1"
-            >
-              <div className="form-group">
-  <label>
-    Primer Nombre:
-    <input
-      type="text"
-      name="firstName"
-      className="mx-2"
-      value={editedData.firstName}
-      placeholder={userData?.firstName || ""}
-      onChange={handleInputChange}
-    />
-  </label>
-</div>
-<div className="form-group">
-  <label>
-    Segundo Nombre:
-    <input
-      type="text"
-      name="secondName"
-      className="mx-2"
-      value={editedData.secondName}
-      placeholder={userData?.secondName || ""}
-      onChange={handleInputChange}
-    />
-  </label>
-</div>
-<div className="form-group">
-  <label>
-    Apellido Paterno:
-    <input
-      type="text"
-      name="surnameM"
-      className="mx-2"
-      value={editedData.surnameM}
-      placeholder={userData?.surnameM || ""}
-      onChange={handleInputChange}
-    />
-  </label>
-</div>
-<div className="form-group">
-  <label>
-    Apellido Materno:
-    <input
-      type="text"
-      name="surnameF"
-      className="mx-2"
-      value={editedData.surnameF}
-      placeholder={userData?.surnameF || ""}
-      onChange={handleInputChange}
-    />
-  </label>
-</div>
-<div className="form-group">
-  <label>
-    Sexo:
-    <input
-      type="text"
-      name="sex"
-      className="mx-2"
-      value={editedData.sex}
-      placeholder={userData?.sex || ""}
-      onChange={handleInputChange}
-    />
-  </label>
-</div>
-<div className="form-group">
-  <label>
-    Estado Civil:
-    <input
-      type="text"
-      name="stateCivil"
-      className="mx-2"
-      value={editedData.stateCivil}
-      placeholder={userData?.stateCivil || ""}
-      onChange={handleInputChange}
-    />
-  </label>
-</div>
-<div className="form-group">
-  <label>
-    Fecha de Nacimiento:
-    <input
-      type="text"
-      name="birthday"
-      className="mx-2"
-      value={editedData.birthday}
-      placeholder={userData?.birthday || ""}
-      onChange={handleInputChange}
-    />
-  </label>
-</div>
-<div className="form-group">
-  <label>
-    Dirección:
-    <input
-      type="text"
-      name="address"
-      className="mx-2"
-      value={editedData.address}
-      placeholder={userData?.address || ""}
-      onChange={handleInputChange}
-    />
-  </label>
-</div>
-<div className="form-group">
-  <label>
-    Email:
-    <input
-      type="text"
-      name="email"
-      className="mx-2"
-      value={editedData.email}
-      placeholder={userData?.email || ""}
-      onChange={handleInputChange}
-    />
-  </label>
-</div>
-<div className="form-group">
-  <label>
-    Teléfono:
-    <input
-      type="text"
-      name="phone"
-      className="mx-2"
-      value={editedData.phone}
-      placeholder={userData?.phone || ""}
-      onChange={handleInputChange}
-    />
-  </label>
-</div>
-<button onClick={handleSaveClick}>Guardar</button>
-
-            </form>
+          <div className="form-group">
+            <label>
+              Primer Nombre:
+              <input
+                type="text"
+                name="firstName"
+                className="mx-2"
+                value={editedData.firstName}
+                placeholder={userData?.firstName || ""}
+                onChange={handleInputChange}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              Segundo Nombre:
+              <input
+                type="text"
+                name="secondName"
+                className="mx-2"
+                value={editedData.secondName}
+                placeholder={userData?.secondName || ""}
+                onChange={handleInputChange}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              Apellido Paterno:
+              <input
+                type="text"
+                name="surnameM"
+                className="mx-2"
+                value={editedData.surnameM}
+                placeholder={userData?.surnameM || ""}
+                onChange={handleInputChange}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              Apellido Materno:
+              <input
+                type="text"
+                name="surnameF"
+                className="mx-2"
+                value={editedData.surnameF}
+                placeholder={userData?.surnameF || ""}
+                onChange={handleInputChange}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              Sexo:
+              <input
+                type="text"
+                name="sex"
+                className="mx-2"
+                value={editedData.sex}
+                placeholder={userData?.sex || ""}
+                onChange={handleInputChange}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              Estado Civil:
+              <input
+                type="text"
+                name="stateCivil"
+                className="mx-2"
+                value={editedData.stateCivil}
+                placeholder={userData?.stateCivil || ""}
+                onChange={handleInputChange}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              Fecha de Nacimiento:
+              <input
+                type="text"
+                name="birthday"
+                className="mx-2"
+                value={editedData.birthday}
+                placeholder={userData?.birthday || ""}
+                onChange={handleInputChange}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              Dirección:
+              <input
+                type="text"
+                name="address"
+                className="mx-2"
+                value={editedData.address}
+                placeholder={userData?.address || ""}
+                onChange={handleInputChange}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              Email:
+              <input
+                type="text"
+                name="email"
+                className="mx-2"
+                value={editedData.email}
+                placeholder={userData?.email || ""}
+                onChange={handleInputChange}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              Teléfono:
+              <input
+                type="text"
+                name="phone"
+                className="mx-2"
+                value={editedData.phone}
+                placeholder={userData?.phone || ""}
+                onChange={handleInputChange}
+              />
+            </label>
+          </div>
+          <button onClick={handleSaveClick}>Guardar</button>
+        </form>
           
           ) : (
             <>
