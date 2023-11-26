5397c150f39678c0f118bef959ab9a5d65f4fc4a
Dylan Choque Nina <113360472+Dylann55@users.noreply.github.com>
2023-11-26 20:13:22 -0300

    outdate

--- src/router/administrativeRouter/director/directorRouter.jsx
+++ src/router/administrativeRouter/director/directorRouter.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../../../Pages/administrative/director/home';
 import { Users } from '../../../Pages/administrative/director/users/users';
-import { Academic } from '../../../Pages/administrative/director/academic/academic';
-import { Student } from '../../../Pages/administrative/director/student/student';
-import { Document } from '../../../Pages/administrative/director/student/document/document';
-import { HandleTitle } from '../../../Pages/administrative/director/student/handleTitle/handleTitle';
 
 const DirectorRouter = () => {
   return (
@@ -13,10 +9,6 @@ const DirectorRouter = () => {
       <Route path="/">
         <Route index element={<Home />} />
         <Route path="Users" element={<Users />} />
-        <Route path="Academic" element={<Academic />} />
-        <Route path="Student" element={<Student />} />
-        <Route path="Document/:roleHasUserID" element={<Document />} />
-        <Route path="HandleTitle/:userID" element={<HandleTitle />} />
       </Route>
     </Routes>
   );
--- src/router/administrativeRouter/mandated/mandatedRouter.jsx
+++ src/router/administrativeRouter/mandated/mandatedRouter.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../../../Pages/administrative/mandated/home';
 import { Users } from '../../../Pages/administrative/mandated/users/users';
-import { Academic } from '../../../Pages/administrative/mandated/academic/academic';
-import { Student } from '../../../Pages/administrative/mandated/student/student';
-import { Document } from '../../../Pages/administrative/mandated/student/document/document';
-import { HandleTitle } from '../../../Pages/administrative/mandated/student/handleTitle/handleTitle';
 
 const MandatedRouter = () => {
   return (
@@ -13,11 +9,6 @@ const MandatedRouter = () => {
       <Route path="/">
         <Route index element={<Home />} />
         <Route path="Users" element={<Users />} />
-        <Route path="Academic" element={<Academic />} />
-        <Route path="Student" element={<Student />} />
-        <Route path="Document/:roleHasUserID" element={<Document />} />
-        <Route path="HandleTitle/:userID" element={<HandleTitle />} />
-      
       </Route>
     </Routes>
   );
